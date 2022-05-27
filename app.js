5104f461e2aec03b2365615d06bc47139a7ce34b
TheMatrixL <driving_pagodas_0v@icloud.com>
2022-05-27 23:57:15 +0100

    add tick to delete function

--- app.js
+++ app.js
@@ -23,58 +23,84 @@ const itemSchema = new mongoose.Schema ({  //create Schema (like data structure)
 const Item = mongoose.model("Item", itemSchema);
 
 const item1 = new Item({
-  name: "this is item1",
+  name: "This is item1",
   });
 
 const item2 = new Item({
-  name: "hit the button to add more",
+  name: "Hit the button to add more",
   });
 
 const item3 = new Item({
-  name: "tick the box to delete",
+  name: "Tick the box to delete",
   });
 
-const defaultItems = [item1, item2, item3];
-
-Item.insertMany(defaultItems, function(err) {
-  if (err) {
-    console.log("err");
-  } else {
-    console.log("Successfully add all default items");
-  }
-})
-
-
+const defaultItems = [item1, item2, item3]; //in oder to insert many items
 
 
 
 app.get('/', function(req, res) {
 
-  Item.find({}, function(err, fundItems) { //{}meaneverything
-    if (err) {
-      console.log("err");
-    } else {console.log(fundItems)}
+  let day = date.getDate();
+
+  Item.find({}, function(err, foundItems){ //{}meaneverything
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log("err");
+        } else {
+          console.log("Successfully add all default items");
+        }
+      })
+      res.redirect('/');
+    } else {res.render('list', {listTitle: day, newListItems: foundItems})}
+
   });
 
-  let day = date.getDate(); // call the function that was created by us
-
-  res.render('list', {listTitle: day, newListItems: items}); // listTitle and newListItems are refer to HTML ejs code
+  // res.render('list', {listTitle: day, newListItems: fouundList}); // listTitle and newListItems are refer to HTML ejs code
 
 });
 
 app.post("/", function(req, res){
 
-  let item = req.body.newItem;
+  let itemName = req.body.newItem;
 
-  console.log(req.body);
-  if (req.body.list === "Work") {  // use if statement to check listTitle and change post to "/" or "/work"
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+  const item = new Item({
+    name: itemName
+    });
+
+  item.save()
+  res.redirect("/");
+
+  // if (req.body.list === "Work") {  // use if statement to check listTitle and change post to "/" or "/work"
+  //   workItems.push(item);
+  //   res.redirect("/work");
+  // } else {
+  //   items.push(item);
+  //   res.redirect("/");
+  // }
+
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox
+  let itemName = ""
+
+  Item.findById(checkedItemId, function(err, item){
+    if(!err) {
+      itemName = item.name;
   }
+  });
 
+  Item.findByIdAndRemove(checkedItemId, function (err){
+    if (err){
+        console.log(err)
+    }
+    else{
+        console.log("(" + itemName + ")" + " has been delete!")
+    }
+    res.redirect("/")
+  })
 });
 
 app.get('/work', function(req, res) {
