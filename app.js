49253d18659773768f27bc7ff6dcd1bff453a011
TheMatrixL <driving_pagodas_0v@icloud.com>
2022-05-30 18:58:07 +0100

    link to mongo altas could DB

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
+require('dotenv').config()
+
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
- 
+
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,7 +18,7 @@ app.use(express.static(__dirname + "/public")); // static to link local file
 
 
 // create databse
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect(process.env.MONGODB_ATLAS);
 
 const itemSchema = new mongoose.Schema({ //create itemSchema
   name: String,
